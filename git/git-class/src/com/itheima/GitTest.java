58385c6067068b1cf315e1dbea8940c6beff1961
zhaozhun <1436535149@qq.com>
2019-03-22 18:40:15 +0800

    把本地工程添加到git

---
+++ git/git-class/src/com/itheima/GitTest.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+
+
+    }
+}
